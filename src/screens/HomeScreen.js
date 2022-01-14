387c19f536e06381114640c3be9baa2af6f0f668
ashiljayaraj <ashiljayaraj@gmail.com>
2022-01-14 21:57:33 +0530

    First commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (<div style={{ width: '200px', height: '400px', background: 'gray', border: '4px dashed red' }}>
+  <Button onPress={() => { console.log('Button Clickeddd' )}}
+  accessibilityLabel="Learn more about this purple button"
+   title="Learn More" style={styles.text} value={'ASHIL'}>HomeScreen</Button>
+  <Text style={styles.text}>Menu</Text>
+  <Text style={styles.text}>Login</Text>
+  <Text style={styles.text}>LOGOUT</Text>
+  </div>
+  );
 };
 
 const styles = StyleSheet.create({
